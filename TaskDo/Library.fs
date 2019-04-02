fd47d0608d46c8a9b50d8be4b5637e5359ee4753
mwyrebski <mwyrebski@users.noreply.github.com>
2019-04-02 22:37:16 +0200

    Add TaskList and enrich Task modules

--- TaskDo/Library.fs
+++ TaskDo/Library.fs
@@ -20,12 +20,29 @@ type Task =
     | Todo of TaskData
     | Subtask
 
-type TaskList = Task list
+type TaskList = {
+    Name: string
+    Tasks: Task list
+    }
 
+
+module TaskList =
+    
+    let create name =
+        {Name = name; Tasks = []}
+
+    let addTask task tasklist =
+        {tasklist with Tasks = task :: tasklist.Tasks}
 
 
 module Task =
-    
+
     let create title =
         {Title = title; Status = Undone}
+
+    let changeTitle title task =
+        {task with Title = title}
+
+    let markDone task =
+        {task with Status = Done}
 
