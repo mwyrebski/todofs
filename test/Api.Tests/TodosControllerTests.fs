a5bb78ce2717a726e8bc2e1b3fdc172ac557e63a
mwyrebski <mwyrebski@users.noreply.github.com>
2019-11-03 23:23:51 +0100

    Add simple test for GetTodos

--- test/Api.Tests/TodosControllerTests.fs
+++ test/Api.Tests/TodosControllerTests.fs
@@ -1,8 +1,21 @@
 module TodoFs.Api.TodosController.Tests
 
-open System
+open Microsoft.AspNetCore.Mvc
+open TodoFs.Api.Controllers
+open Microsoft.Extensions.Caching.Memory
+open TodoFs.Api.Data
 open Xunit
+open FsUnit.Xunit
+
+
+let repo = new TodosRepository(new MemoryCache(MemoryCacheOptions()))
+
 
 [<Fact>]
-let ``My test`` () =
-    Assert.True(true)
+let ``Returns OkObjectResult with EnvelopeDto from GetTodos``() =
+    let sut = TodosController repo
+
+    let actual = sut.GetTodos()
+
+    actual |> should be ofExactType<OkObjectResult>
+    (actual :?> OkObjectResult).Value |> should be ofExactType<EnvelopeDto>
