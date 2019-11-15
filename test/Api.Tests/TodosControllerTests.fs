624ec161ddf93f731c92ca4dd2dfe2569fb71890
mwyrebski <mwyrebski@users.noreply.github.com>
2019-11-16 00:44:14 +0100

    Add tests for `GetTodos()`

--- test/Api.Tests/TodosControllerTests.fs
+++ test/Api.Tests/TodosControllerTests.fs
@@ -1,21 +1,42 @@
 module TodoFs.Api.TodosController.Tests
 
 open Microsoft.AspNetCore.Mvc
-open TodoFs.Api.Controllers
 open Microsoft.Extensions.Caching.Memory
+open Swensen.Unquote
+open TodoFs.Api.Controllers
 open TodoFs.Api.Data
+open TodoFs.Domain
+open TodoFs.Domain.Implementation
 open Xunit
-open FsUnit.Xunit
 
 
 let repo = new TodosRepository(new MemoryCache(MemoryCacheOptions()))
+let sut = TodosController repo
 
 
-[<Fact>]
-let ``Returns OkObjectResult with EnvelopeDto from GetTodos``() =
-    let sut = TodosController repo
+module GetTodos =
 
-    let actual = sut.GetTodos()
+    let private extractTodos (actionResult: IActionResult) =
+        let result = actionResult :?> OkObjectResult
+        let envelope = result.Value :?> EnvelopeDto
+        envelope.Todos
 
-    actual |> should be ofExactType<OkObjectResult>
-    (actual :?> OkObjectResult).Value |> should be ofExactType<EnvelopeDto>
+    [<Fact>]
+    let ``Returns OkObjectResult with EnvelopeDto with empty value``() =
+        let todos = sut.GetTodos() |> extractTodos
+
+        todos =! []
+
+    [<Fact>]
+    let ``Returns expected Todo from GetTodos``() =
+        let todo = Name "todo1" |> createTodo
+        todo |> repo.Upsert
+
+        let todos = sut.GetTodos() |> extractTodos
+
+        List.length todos =! 1
+        let head = List.head todos
+        head.Id =! todo.Id.Value
+        head.Name =! todo.Name.Value
+        head.Tasks =! []
+
